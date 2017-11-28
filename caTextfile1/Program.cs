d3b4a602f3c59e2be9aefafabf315c74d7d6e741
Anja Birkelund <anbi@herningsholm.dk>
2017-11-28 10:30:52 +0100

    Add project files.

---
+++ caTextfile1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace caTextfile1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Read();
+        }
+        public static string GetLine()
+        {
+            Console.Write("? ");
+            return Console.ReadLine();
+        }
+
+        public static void PutLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public static void Write()
+        {
+            StreamWriter writer = new StreamWriter("Tekst.txt"); // C:\\Users\\ANBI\\Documents\\Test
+            string line = GetLine();
+            while (line.Length > 0)
+            {
+                writer.WriteLine(line);
+                line = GetLine();
+            }
+            writer.Close();
+        }
+
+        public static void Read()
+        {
+            StreamReader reader = new StreamReader("Tekst.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                PutLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+    }
+}
